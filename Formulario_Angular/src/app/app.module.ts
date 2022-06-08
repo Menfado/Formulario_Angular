cc89ba336e34c8261e7521476087bc675e9520f2
Carmen Almonacid Martos <carmen@Carmens-MacBook-Pro.local>
2022-06-08 18:09:42 +0200

    Formulario_Angular

---
+++ Formulario_Angular/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { RegisterComponent } from './components/register/register.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    RegisterComponent
+   
+  ],
+  imports: [
+    BrowserModule,
+    ReactiveFormsModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
